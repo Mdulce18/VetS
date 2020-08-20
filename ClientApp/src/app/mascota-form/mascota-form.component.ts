8cb602fb9ee7258e37c33073d1979a92ef18e987
jscampos <seba.ar@gmail.com>
2020-08-20 18:44:57 -0300

    aplicando cambios al componente

--- ClientApp/src/app/mascota-form/mascota-form.component.ts
+++ ClientApp/src/app/mascota-form/mascota-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AnimalService } from '../../services/animal.service';
+import { MascotaService } from '../../services/mascota.service';
 
 @Component({
     selector: 'app-mascota-form',
@@ -8,19 +8,41 @@ import { AnimalService } from '../../services/animal.service';
 })
 /** mascota-form component*/
 export class MascotaFormComponent implements OnInit {
+  nombre: any;
   animales: any;
-  razas: any [];
-  mascota: any = {};
+  razas: any;
+  fechaNacimiento: Date;
+  mascota: any = {}
+  ;
 
   /** mascota-form ctor */
-  constructor(private animalService: AnimalService) {
+  constructor(private mascotaService: MascotaService) {
 
-    }
+  }
+
   ngOnInit() {
-    this.animalService.getAnimales().subscribe(animales => this.animales = animales);
+    this.mascotaService.getAnimales().subscribe(animales => this.animales = animales);
   }
+
+  alIngresarNombre() {
+    this.mascota.nombre = this.nombre;
+  }
+
   alCambiarAnimal() {
-    var animalSeleccionado = this.animales.find(a => a.id == this.mascota.animal);
+    var animalSeleccionado = this.animales.find(a => a.id == this.mascota.animalId);
     this.razas = animalSeleccionado ? animalSeleccionado.razas : [];
+    delete this.mascota.razaId;
   }
+
+  alSelFNacimiento() {
+    this.mascota.fechaNacimiento = this.fechaNacimiento;
+
+  }
+
+  Guardar() {
+    this.mascotaService.createMascota(this.mascota)
+      .subscribe(x => console.log(x));
+  }
+
+
 }
