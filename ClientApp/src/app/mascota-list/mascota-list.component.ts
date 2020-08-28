0ed624403c0ddfd6438ea5bc866a216063d5ca6c
jscampos <seba.ar@gmail.com>
2020-08-28 17:22:25 -0300

    Falta mapear nombres en lugar de Id

--- ClientApp/src/app/mascota-list/mascota-list.component.ts
+++ ClientApp/src/app/mascota-list/mascota-list.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { MascotaService } from '../../services/mascota.service';
 import { Mascota } from '../../models/mascota';
+import { KeyValuePair } from '../../models/keyValuePair';
 
 @Component({
     selector: 'app-mascota-list',
     templateUrl: './mascota-list.component.html',
-    styleUrls: ['./mascota-list.component.scss']
+    styleUrls: ['./mascota-list.component.css']
 })
 /** mascota-list component*/
 export class MascotaListComponent implements OnInit {
-  mascotas: any =[];
+  mascotas: any = [];
+  animales: any = [];
+  filtro: any = {};
 
   constructor(private mascotaService: MascotaService) { }
 
   ngOnInit() {
+    this.mascotaService.getAnimales().subscribe(animales => this.animales = animales);
+
     this.mascotaService.getMascotas().subscribe(mascotas => this.mascotas = mascotas);
-    }
+  }
+
+  alCambiarFiltro() {
+
+  }
+
 }
